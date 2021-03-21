4a7c5b189a4f8e23755e153b1058dfa9b6e27140
rival98 <80638225+rival98@users.noreply.github.com>
2021-03-21 18:10:07 +0100

    Creator: First commit, with all dependencies
    First ever commit, with all dependencies needed for script, and of course - main script.

---
+++ scriptfiles/entrances/entrance_00.cr
@@ -0,0 +1,2 @@
+(Rival) Cochrane Dam: CreateDynamicPickup(19198, 6, 7104.464843, 8696.280273, 35.489063, 0, 0);
+(Rival) Cochrane Dam: CreateDynamicPickup(19198, 6, -959.620178, 1953.993041, 9.000000, 0, 17);
